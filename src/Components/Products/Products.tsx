a273a8e333372620bfd8921b6ff5c6c93a7d93d2
Iryna-Don <idonets0104081518@gmail.com>
2024-05-24 13:13:43 +0200

    tried to implement pagination

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -3,41 +3,62 @@ import Product, {IProductProps} from "./Product";
 import styles from "./Products.module.css";
 
 const Products: FC<any> = () => {
+    const [skip, setSkip] = useState(0);
+    let linkText = 'https://dummyjson.com/products?limit=30&skip=' + skip;
+    const [link, setLink] = useState(linkText);
     const [products, setProducts] = useState<IProductProps[]>([]);
+    const [total, setTotal] = useState<number>(0);
     useEffect(() => {
-        fetch('https://dummyjson.com/products')
+        fetch(link)
             .then(value => value.json())
-            .then(({products}) => {
+            .then(({products, total}) => {
                 setProducts(products);
+                setTotal(total);
             });
-    }, [])
+    }, [link])
     return (
-        <div className={styles.generalDiv}>
-            {products.map(({
-                               id,
-                               title,
-                               description,
-                               price,
-                               discountPercentage,
-                               rating,
-                               stock,
-                               brand,
-                               category,
-                               thumbnail,
-                               images
-                           }: IProductProps) => <Product key={id} id={id}
-                                                         title={title}
-                                                         description={description}
-                                                         price={price}
-                                                         discountPercentage={discountPercentage}
-                                                         rating={rating}
-                                                         stock={stock}
-                                                         brand={brand}
-                                                         category={category}
-                                                         thumbnail={thumbnail}
-                                                         images={images}
-            />)}
-        </div>
+        <>
+            <button disabled={skip <= 0}
+            onClick={() => {
+                setSkip(skip - 30);
+                setLink(linkText);
+            }}>Previous 30 Cards
+            </button>
+
+            <button disabled={skip >= total}
+                    onClick={() => {
+                setSkip(skip + 30);
+                setLink(linkText);
+            }}>Next 30 Cards
+            </button>
+
+            <div className={styles.generalDiv}>
+                {products.map(({
+                                   id,
+                                   title,
+                                   description,
+                                   price,
+                                   discountPercentage,
+                                   rating,
+                                   stock,
+                                   brand,
+                                   category,
+                                   thumbnail,
+                                   images
+                               }: IProductProps) => <Product key={id} id={id}
+                                                             title={title}
+                                                             description={description}
+                                                             price={price}
+                                                             discountPercentage={discountPercentage}
+                                                             rating={rating}
+                                                             stock={stock}
+                                                             brand={brand}
+                                                             category={category}
+                                                             thumbnail={thumbnail}
+                                                             images={images}
+                />)}
+            </div>
+        </>
     );
 };
 export default Products;
