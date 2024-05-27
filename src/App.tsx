6e955ef35cca538c238804f2bfd42c998d9874cd
Iryna-Don <idonets0104081518@gmail.com>
2024-05-27 21:08:33 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 
 const App = () => {
     return (
         <div>
-            text
+            
         </div>
     );
-}
+};
 
 export default App;
